1f80663c1cafface49e3365e233d39fe11b016d3
ByLuis360 <usr2477@salesianos-lacuesta.net>
2025-02-26 10:09:13 +0000

    fix-collisions
    Me faltaba conectar los respectivos nodos y poner la bala en el grupo bullet

--- scenes/enemy_ship.gd
+++ scenes/enemy_ship.gd
@@ -1,6 +1,24 @@
 extends Node2D
 
+@onready var _animated_sprite = $Area2D/enemy
+@onready var _animation_config = $Area2D/AnimationPlayer
 
+const ANIMATION_STATES = {
+	MOVE = "enemy", DESTROYED = "destroy"
+}
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var enemy_move = ANIMATION_STATES.MOVE
+var enemy_destroyed = ANIMATION_STATES.DESTROYED
+
+
+func _on_ready() -> void:
+	_animated_sprite.play(enemy_move)
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("bullet"):
+		#var level = get_parent()
+		#level.add_score(30)
+		queue_free()
+		
+	
---
+++ scenes/enemy_ship_2.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var _animated_sprite = $Area2D/AnimatedSprite2D
+
+const ANIMATION_STATES = {
+	MOVE = "enemy_move2" #DESTROYED = "destroy"
+}
+
+func _on_ready() -> void:
+	_animated_sprite.play(ANIMATION_STATES.MOVE)
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("bullet"):
+		#var level = get_parent()
+		#level.add_score(10)
+		queue_free()
